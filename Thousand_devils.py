e4047a79f3b59614379d3921687b678f78d20a9a
MaxMavr <maxmavr2005@gmail.com>
2023-11-13 01:02:50 +0300

    Тест 3

--- Thousand_devils.py
+++ Thousand_devils.py
@@ -6,19 +6,6 @@ import pygame
 # Нужно сделать:
 # 1) починить выбор клеток (Иногда не выбираются клетки после стрелок)
 # {Функции: check_step, check_step_arrow}
-# Blb djehufgwe
-# Blb djehufgwe
-# Blb djehufgwe
-# Blb djehufgwe
-# Blb djehufgwe# Blb djehufgwe# Blb djehufgwe# Blb djehufgwe
-# Blb djehufgwe
-# Blb djehufgwe
-# Blb djehufgwe
-
-
-
-
-
 
 window_height = 1000
 window_width = window_height * 16 / 9
